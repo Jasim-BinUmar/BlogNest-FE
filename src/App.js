dceda2c4bfcc20f5ae2cc0247425ca061a75176c
Jasim-BinUmar <jasimbinumar@gmail.com>
2024-10-28 14:49:47 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUp from './(auth)/signUp';
+import SignIn from './(auth)/signIn';
+import HomePage from './(pages)/HomePage';
+import BlogsPage from './(pages)/blogsPage';
+import MyBlogs from './(pages)/myBlogs';
+import LandingPage from './(pages)/landingPage';
+import BlogView from './(pages)/blogView';
+import EditBlog from './(pages)/editBlog';
+import CreateBlog from './(pages)/createBlog';
+import ProtectedRoute from './util/ProtectedRoute';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/homepage" element={<ProtectedRoute element={HomePage} />} />
+        <Route path="/blogs" element={<ProtectedRoute element={BlogsPage} />} />
+        <Route path="/myblogs" element={<ProtectedRoute element={MyBlogs} />} />
+        <Route path="/blogs/:id" element={<ProtectedRoute element={BlogView} />} />
+        <Route path="/editblog/:id" element={<ProtectedRoute element={EditBlog} />} />
+        <Route path="/createBlog" element={<ProtectedRoute element={CreateBlog} />} />
+      </Routes>
+    </Router>
   );
 }
 
