3acd18aa92e5ae28aa0db6b7b00f652e38aecf82
Jasim <jasimbinumar@gmail.com>
2024-10-29 04:33:45 +0500

    Update signUp.jsx

--- src/(auth)/signUp.jsx
+++ src/(auth)/signUp.jsx
@@ -22,7 +22,7 @@ const SignUp = () => {
     };
 
     try {
-      const response = await fetch('https://blog-nest-fe.vercel.app/api/auth/register', {
+      const response = await fetch('https://blog-nest-be.vercel.app/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
